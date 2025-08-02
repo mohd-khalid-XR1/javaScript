297fdf9100a4e8f64d1450c6a15a32feaad2e486
Your Name <you@example.com>
2025-08-02 20:11:16 +0530

    api

---
+++ practice.js
@@ -0,0 +1,22 @@
+const producsts = [
+    {
+        id : 3,
+        name : 'fsdlfj',
+        price : 493,
+    },
+     {
+        id : 3,
+        name : 'fsdlfj',
+        price : 493,
+    },
+     {
+        id : 3,
+        name : 'fsdlfj',
+        price : 493,
+    },
+     {
+        id : 3,
+        name : 'fsdlfj',
+        price : 493,
+    }
+]
