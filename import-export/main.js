cef79120456fda7404621d1131b4405186be8316
Your Name <you@example.com>
2025-08-19 19:17:35 +0530

    import-export

---
+++ import-export/main.js
@@ -0,0 +1,5 @@
+import {a , greet} from './script.js'
+import sum from './script.js';
+console.log(a);
+greet()
+console.log(sum(3,5));
---
+++ import-export/script.js
@@ -0,0 +1,10 @@
+const a = 5
+function greet() {
+    console.log("Hello");
+}
+const sum = (a, b) => {
+    return a + b
+}
+
+export { a, greet }
+export default sum
